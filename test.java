576f4bee7395433af5bc5a782e525f2c9042c399
Nurafni23 <afni230304@gmail.com>
2025-03-07 20:30:38 -0800

    Add files via upload

---
+++ test.java
@@ -0,0 +1,9 @@
+package com.example.mandiri;
+
+public class test {
+    public static void main(String[] args) {
+        Database db = new Database();
+        db.open();
+        db.view();
+    }
+}
